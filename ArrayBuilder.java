6f47b59ddf27472defbefa0558d4e82a4da3bc19
Hpuma <pumahenry@live.com>
2019-10-07 23:58:18 -0400

    new_side_project_Array_Builder_needs_testing_and_debugging

---
+++ ArrayBuilder.java
@@ -0,0 +1,90 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class ArrayBuilder{
+    // TESTING
+    public static void main(String[] args) {
+        
+    }
+
+    public static int []Data;
+
+    ArrayBuilder(){
+
+    }
+    ArrayBuilder(int []InputArray){
+        Data = InputArray;
+    }
+
+    public int[] ShuffleArray(){
+        int DataLength = Data.length;
+        int []ShuffledArray = new int[DataLength];
+
+        HashMap<Integer,Boolean> IndexTracker = new HashMap<Integer,Boolean>();
+
+        int min = 0;
+        int max = DataLength - 1;
+
+        int Index = 0;
+        int RandomIndex = 0;
+        Boolean SearchIndex = true;
+
+        while(Index < DataLength){
+            SearchIndex = true;
+            while(SearchIndex){
+                RandomNumber = (int)((Math.random()*((max-min)+1))+min); 
+                if ((IndexTracker.get(RandomIndex) == null) && (RandomIndex >= min)  && (RandomIndex <= max)){
+                    ShuffledArray[Index] = Data[RandomIndex];
+                    IndexTracker.put(RandomIndex, true);
+                    SearchIndex = false;
+                }
+            }
+            Index+=1;
+        }
+        return ShuffledArray;
+    }
+
+    public int[] RandomArray(int Size, int LeftBound, int RightBound){
+        int []NewArray = new int[Size];
+        int Index = 0;
+        int RandomNumber = 0;
+        
+        while(Index < Size){
+            NewArray = (int)((Math.random()*((RightBound-LeftBound)+1))+LeftBound); 
+            NewArray[Index] = RandomNumber;
+            Index+=1;
+        }
+        return NewArray;
+    }
+
+
+    public int[] NoRepeatArray(int Size, int LeftBound, int RightBound){
+
+        int [] NewArray = new int[Size];
+
+        HashMap<Integer,Boolean> IndexTracker = new HashMap<Integer,Boolean>();
+
+        int Index = 0;
+        int RandomNumber = 0;
+        Boolean SearchIndex = true;
+
+        while(Index < DataLength){
+            SearchIndex = true;
+            while(SearchIndex){
+                RandomNumber = (int)((Math.random()*((max-min)+1))+min); 
+                if ((IndexTracker.get(RandomNumber) == null) && (RandomNumber >= LeftBound)  && (RandomNumber <= RightBound)){
+                    NewArray[Index] = RandomNumber;
+                    IndexTracker.put(RandomNumber, true);
+                    SearchIndex = false;
+                }
+            }
+            Index+=1;
+        }
+        return NewArray;
+    }
+
+    public static void printArray(){
+        System.out.print(Arrays.toString(Data));
+    }
+
+}
