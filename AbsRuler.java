87675beee6c45e9289c9f27932483c4caeb1ae59
Henry_Puma <pumahenry@live.com>
2019-10-11 12:46:08 -0400

    added_comments_to_AbsRuler

--- AbsRuler.java
+++ AbsRuler.java
@@ -8,6 +8,9 @@ class AbsRuler{
         System.out.print(Arrays.toString(testAns));
     }
     public static int[] absSort(int[] line){
+        // Check if the array is less than 2 which is the easiest case.
+        // If it is empty than return null, if it size 1 then do nothing. 
+        // If the Array is length 2 then compare those two and eliminate accordingly.
         if (line.length  <= 2){
             switch(line.length){
                 case 0: return null;
@@ -15,19 +18,24 @@ class AbsRuler{
                 case 2: if(line[1] < line[0]) return Arrays.copyOf(line,1);
             }
         }
+        // ArrayList: used to build the new array that contains all the elements that make the array sorted.
         ArrayList<Integer> survivors = new ArrayList<Integer>();
-        int lastGuy = line.length-1;
-        for(int i = 0; i < lastGuy; i++){
+        // LastGuy: stores the second to last possible index of the array.
+        int LastGuy = line.length-1;
+        // Compares the current element with the next element and checks if those two elements are sorted.
+        for(int i = 0; i < LastGuy; i++){
             if(line[i] < line[i+1] ) {
                 survivors.add(line[i]);
             }
         }
-        if(line[lastGuy] > survivors.get(survivors.size()-1)){
-            survivors.add(line[lastGuy]);
-            if(line[lastGuy-1] > survivors.get(survivors.size()-1)){
-                survivors.add(line[lastGuy-1]);
+        // Looks at the very last element of the array and checks to see if it is greater than the second to last element of the array.
+        if(line[LastGuy] > survivors.get(survivors.size()-1)){
+            survivors.add(line[LastGuy]);
+            if(line[LastGuy-1] > survivors.get(survivors.size()-1)){
+                survivors.add(line[LastGuy-1]);
             }
         }
+        // Returns the survivor list as an array.
         return survivors.stream().mapToInt(i -> i).toArray(); // MAGIC 
         //REF: https://stackoverflow.com/questions/718554/how-to-convert-an-arraylist-containing-integers-to-primitive-int-array/718606
     }
