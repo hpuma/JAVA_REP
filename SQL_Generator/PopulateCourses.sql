c959dd78a997b8b43b2f37d8934fa1b09ae42af6
Henry_Puma <pumahenry@live.com>
2019-12-05 17:05:39 -0500

    Performance_improvements_and_overloaded_InsertData_method

--- SQL_Generator/PopulateCourses.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO courses VALUES (22000, "Algorithms", "CSC");
-INSERT INTO courses VALUES (22100, "SoftwareDesign", "CSC");
-INSERT INTO courses VALUES (21200, "DataStructures", "CSC");
-INSERT INTO courses VALUES (21100, "ComputerFundamentals", "CSC");
-INSERT INTO courses VALUES (21700, "Statistics", "CSC");
--- SQL_Generator/PopulateTable.java
+++ SQL_Generator/PopulateTable.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class PopulateTable {
+    // TESTING
     public static void main(String[] args) throws IOException {
+        // OLD INSERT
         PopulateTable test = new PopulateTable("students", 100);
         String[] sourceData = {"studentID.txt","firstName.txt","lastName.txt","sex.txt"};
         System.out.println(test.InsertData("PopulateStudents.sql", "StudentsData",sourceData));
+        // NEW INSERT
+        // PopulateTable test = new PopulateTable("students", 100);
+        // System.out.println(test.InsertData("PopulateStudents.sql", "StudentsData"));
     }
     private String tableName;
     private String workingDirectory;
@@ -28,6 +34,7 @@ public class PopulateTable {
         this.allFileLines = null;
         this.thisFileLines = null;
     }
+    // Clears the contents of the specific file
     private void clearFile(String filePath) throws IOException {
         FileWriter fw = new FileWriter(filePath, false); 
         PrintWriter pw= new PrintWriter(fw, false);
@@ -35,43 +42,55 @@ public class PopulateTable {
         pw.close();
         fw.close();
     }
+    // Checks to see if the column data is text, if so then surround it with quotation marks
     private String format(String str){
-        if(str != null && str.matches("^[a-zA-Z]*$")){
-            return "\"" +str + "\"";
-        }
+        if(str != null && str.matches("^[a-zA-Z]*$")){ return "\"" +str + "\"";}
         return str;
     }
+    // Creates an SQL file that inserts into a table using the contents of the files in "dataFilesNames"
+    // NOTE: The target file is always cleared before anything
+    // NOTE: The data file names must follow the column order of the table
+    // targetFile: The file name where the SQL statements will be written
+    // dataFolderName: The folder where the .txt files are located in
+    // dataFileNames: Array containing the names of each txt files with the data
     public boolean InsertData(String targetFile, String dataFolderName, String[] dataFileNames){
         try{
-            clearFile(targetFile);
-            allFileLines = new ArrayList<ArrayList<String>>();
-            // Adding the lines of each file into a 2d Array list.
-            String dataPath = workingDirectory + "\\" + dataFolderName +  "\\";
+            clearFile(targetFile); // Clear the target file before writing anything
+            allFileLines = new ArrayList<ArrayList<String>>(); // 2D ArrayList containing the lines of each txt file
+            String dataPath = workingDirectory + "\\" + dataFolderName +  "\\"; // Full path of the dataFolder
+            // Iterates through each file in the dataFileNames array
             for(String dataFile: dataFileNames){
+                // Scanner used to read each file
                 scnr  = new Scanner(new File(dataPath + dataFile));
+                // Array list used to store the lines of the current file
                 thisFileLines = new ArrayList<String>();
+                // Adding all the liens of the file into the array list as strings
                 while(scnr.hasNextLine()){
+                    // Filter out non alphabetic or numeric symbols, then insert in quotation marks if the input is alphabetic
                     thisFileLines.add(format(scnr.nextLine().replaceAll("[^A-Za-z0-9]","")));
                 }
+                // If the file doesn't have the requiered amount of lines, then abort the writing and let the user know
                 if(thisFileLines.size() < numEntries){
                     System.out.println(dataFile + " DOES NOT HAVE "+ numEntries + " ENTRIES");
                     return false;
                 }
+                // Add the lines of the file into the 2D array
                 allFileLines.add(thisFileLines);
             }
-
+            // Setting up the writing process for the target file
             write = new FileWriter(new File(targetFile),true);
 
-            String StmtBuild = "";
-            for(int i = 0; i < numEntries; i++){
-                StmtBuild = "INSERT INTO " + tableName + " VALUES (";
-                for(int j = 0; j < allFileLines.size(); j++){
-                    StmtBuild += allFileLines.get(j).get(i);
-                    if(j != allFileLines.size()-1){
-                        StmtBuild+=", ";
+            // StmtBuild: Used to build each SQL line
+            StringBuilder StmtBuild = new StringBuilder();
+            for(int i = 0; i < numEntries; i++){ // Add numEntries SQL Statements
+                StmtBuild = new StringBuilder( "INSERT INTO " + tableName + " VALUES ("); // Begin the SQL statement
+                for(int j = 0; j < allFileLines.size(); j++){ // Iterate throught the data text files
+                    StmtBuild.append(allFileLines.get(j).get(i));
+                    if(j != allFileLines.size()-1){ // Add Commas where appropriate in the statment
+                        StmtBuild.append(", ");
                     }   
                 }
-                StmtBuild += ");";   
+                StmtBuild.append(");"); // Finalazing the SQL Statement
                 write.write(StmtBuild + System.getProperty("line.separator"));         
             }
             write.close();
@@ -81,6 +100,54 @@ public class PopulateTable {
             System.out.println(error);
         }
         System.out.println("ADDED CONTENTS FROM " + Arrays.toString(dataFileNames)+ " TO \"" + targetFile+"\"");
+        return true;
+    }
+    // This works but it doesn't add the file data in the correct column order, must find a way to insert in the correct order.
+    public boolean InsertData(String targetFile, String dataFolderName){
+        try{
+            clearFile(targetFile); // Clear the target file
+            ArrayList<ArrayList<String> > allFileLines = new ArrayList<ArrayList<String>>();
+            String dataFolderPath = workingDirectory + "\\" + dataFolderName +  "\\";
+            File dataFolder = new File(dataFolderPath);
+            File[] dataFileList = dataFolder.listFiles(new FilenameFilter(){
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.toLowerCase().endsWith(".txt");
+                }
+            });
+            for(File dataFile: dataFileList){
+                scnr = new Scanner(dataFile);
+                thisFileLines = new ArrayList<String>();
+                while(scnr.hasNextLine()){
+                    thisFileLines.add(format(scnr.nextLine().replaceAll("[^A-Za-z0-9]","")));
+                }
+                if(thisFileLines.size() < numEntries){
+                     System.out.println(dataFile + " DOES NOT HAVE "+ numEntries + " ENTRIES");
+                     return false;
+                }
+                allFileLines.add(thisFileLines);
+            }
+            write = new FileWriter(new File(targetFile),true);
+            // String StmtBuild = "";
+            StringBuilder StmtBuild = new StringBuilder("");
+            for(int i = 0; i < numEntries; i++){
+                StmtBuild = new StringBuilder("INSERT INTO " + tableName + " VALUES (");
+                for(int j = 0; j < allFileLines.size(); j++){
+                    StmtBuild.append(allFileLines.get(j).get(i));
+                    if(j != allFileLines.size()-1){
+                        StmtBuild.append(", ");
+                    }   
+                }
+                StmtBuild.append(");"); 
+                write.write(StmtBuild + System.getProperty("line.separator"));         
+            }
+            write.close();
+            System.out.println("ADDED CONTENTS FROM " + Arrays.toString(dataFileList)+ " TO \"" + targetFile+"\"");
+        } catch(FileNotFoundException error){
+            System.out.println(error);
+        } catch(IOException error){
+            System.out.println(error);
+        }
         return true;
     }
 }
