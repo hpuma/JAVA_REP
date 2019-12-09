ed09da1c9d714e4b46d80ff89d5ecacb02747ed7
Henry_Puma <pumahenry@live.com>
2019-12-09 17:36:44 -0500

    Added_Old_Insert_added_new_SQL_Generator_tests

---
+++ OldInsertData.java
@@ -0,0 +1,51 @@
+public class OldInsertData{
+    public static void main(String[] args) {}
+    // This works but it doesn't add the file data in the correct column order, must find a way to insert in the correct order.
+    public boolean InsertData(String targetFile, String dataFolderName){
+        try{
+            clearFile(targetFile); // Clear the target file
+            ArrayList<ArrayList<String> > allFileLines = new ArrayList<ArrayList<String>>();
+            String dataFolderPath = workingDirectory + "\\" + dataFolderName +  "\\";
+            File dataFolder = new File(dataFolderPath);
+            File[] dataFileList = dataFolder.listFiles(new FilenameFilter(){
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.toLowerCase().endsWith(".txt");
+                }
+            });
+            for(File dataFile: dataFileList){
+                scnr = new Scanner(dataFile);
+                thisFileLines = new ArrayList<String>();
+                while(scnr.hasNextLine()){
+                    thisFileLines.add(format(scnr.nextLine().replaceAll("[^A-Za-z0-9]","")));
+                }
+                if(thisFileLines.size() < numEntries){
+                     System.out.println(dataFile + " DOES NOT HAVE "+ numEntries + " ENTRIES");
+                     return false;
+                }
+                allFileLines.add(thisFileLines);
+            }
+            write = new FileWriter(new File(targetFile),true);
+            // String StmtBuild = "";
+            StringBuilder StmtBuild = new StringBuilder("");
+            for(int i = 0; i < numEntries; i++){
+                StmtBuild = new StringBuilder("INSERT INTO " + tableName + " VALUES (");
+                for(int j = 0; j < allFileLines.size(); j++){
+                    StmtBuild.append(allFileLines.get(j).get(i));
+                    if(j != allFileLines.size()-1){
+                        StmtBuild.append(", ");
+                    }   
+                }
+                StmtBuild.append(");"); 
+                write.write(StmtBuild + System.getProperty("line.separator"));         
+            }
+            write.close();
+            System.out.println("ADDED CONTENTS FROM " + Arrays.toString(dataFileList)+ " TO \"" + targetFile+"\"");
+        } catch(FileNotFoundException error){
+            System.out.println(error);
+        } catch(IOException error){
+            System.out.println(error);
+        }
+        return true;
+    }
+}
--- SQL_Generator/CreateSQL.java
+++ SQL_Generator/CreateSQL.java
@@ -1,17 +1,58 @@
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class CreateSQL{
     public static void main(String[] args) {
+        try{
+            // Populate Table Setup for getting the file data, and generating the SQL statements
+            PopulateTable students = new PopulateTable("students", 100);
+            PopulateTable courses = new PopulateTable("courses", 5);
+            PopulateTable classes = new PopulateTable("classes", 100);
+            // File names of the data for the columns of each table
+            String[] studentFiles = {"studentID.txt","firstName.txt","lastName.txt","sex.txt"};
+            String[] courseFiles ={"courseID.txt","courseTitle.txt","department.txt"};
+            String[] classFiles = {"classCode.txt","courseID.txt","studentID.txt","year.txt","semester.txt","GPA.txt"};
 
-        PopulateTable students = new PopulateTable("students", 100);
-        PopulateTable courses = new PopulateTable("courses", 5);
-        PopulateTable classes = new PopulateTable("classes", 100);
+            // Used to connect to the database and pass all queries
+            StatementQuery connect = new StatementQuery("jdbc:mysql://localhost/studentdb?","root","89a55cc5d12E3$$$");
 
-        String[] studentFiles = {"studentID.txt","firstName.txt","lastName.txt","sex.txt"};
-        String[] courseFiles ={"courseID.txt","courseTitle.txt","department.txt"};
-        String[] classFiles = {"classCode.txt","courseID.txt","studentID.txt","year.txt","semester.txt","GPA.txt"};
+            // Retrieving the data for each table based on the files provided
+            ArrayList<ArrayList<String>> AllStudentLines = students.getFileLines("src\\StudentsData",studentFiles);
+            ArrayList<ArrayList<String>> AllCourseLines = courses.getFileLines("src\\CoursesData",courseFiles);
+            ArrayList<ArrayList<String>> AllClassLines = classes.getFileLines("src\\ClassesData",classFiles);
 
-        System.out.println(students.InsertData("PopulateStudents.sql", "StudentsData", studentFiles));
-        System.out.println(courses.InsertData("PopulateCourses.sql", "CoursesData", courseFiles));
-        System.out.println(classes.InsertData("PopulateClasses.sql", "ClassesData", classFiles));
+            // Generating the INSERT INTO table statements based on the file's data
+            ArrayList<String> AllStudentStatements = students.getSQLStatements(AllStudentLines);
+            ArrayList<String> AllCourseStatements = courses.getSQLStatements(AllCourseLines);
+            ArrayList<String> AllClassStatements = classes.getSQLStatements(AllClassLines);
+
+            // Executing all the SQL Statements and applying it for each table in the database
+            connect.executeListStatements(AllStudentStatements);
+            connect.executeListStatements(AllCourseStatements);
+            connect.executeListStatements(AllClassStatements);
+
+            // Generate the SQL Statements into the files from the SQL statements above
+
+            //  System.out.println(students.InsertData("PopulateStudents.sql", "StudentsData", studentFiles));
+            //  System.out.println(courses.InsertData("PopulateCourses.sql", "CoursesData", courseFiles));
+            //  System.out.println(classes.InsertData("PopulateClasses.sql", "ClassesData", classFiles));
+
+        } catch(FileNotFoundException ex){
+            System.out.println(ex);
+        } catch (SQLException ex){
+            System.out.println("SQLException: " + ex.getMessage());
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex){
+            System.out.println("Exception: " + ex.getMessage());
+            ex.printStackTrace();
+        } catch (NullPointerException ex){
+            System.out.println("Exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+
+
+
         
     }
 }
